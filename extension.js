992dbc5813e3056a4a14c7ad7a63c4ed5a6020f5
Simo Aleksandrov <simo3003@me.com>
2018-07-13 17:40:07 +0300

    Format extension.js

--- extension.js
+++ extension.js
@@ -1,75 +1,61 @@
 
-var vscode = require( 'vscode' ),
-    fs = require( 'fs-extra' ),
-    os = require( 'os' ),
-    path = require( 'path' );
+let vscode = require('vscode'),
+    fs = require('fs-extra'),
+    os = require('os'),
+    path = require('path');
 
-function activate( context )
-{
-    function copyConfig()
-    {
-        var source = vscode.workspace.getConfiguration( 'global-config' ).get( 'folder' );
+function activate(context) {
+    function copyConfig() {
+        let source = vscode.workspace.getConfiguration('global-config').get('folder');
 
-        if( source.startsWith( "~" ) )
-        {
-            source = path.join( os.homedir(), source.substr( 1 ) );
+        if (source.startsWith("~")) {
+            source = path.join(os.homedir(), source.substr(1));
         }
 
-        var workspacePath;
-        if( vscode.workspaceFolders && vscode.workspaceFolders.length > 0 )
-        {
-            workspacePath = vscode.workspace.workspaceFolders[ 0 ];
+        let workspacePath;
+        if (vscode.workspaceFolders && vscode.workspaceFolders.length > 0) {
+            workspacePath = vscode.workspace.workspaceFolders[0];
         }
-        if( workspacePath === undefined )
-        {
+        if (workspacePath === undefined) {
             workspacePath = vscode.workspace.rootPath;
         }
 
-        var destination = path.join( workspacePath, ".vscode" );
-        fs.ensureDirSync( destination );
+        let destination = path.join(workspacePath, ".vscode");
+        fs.ensureDirSync(destination);
 
-        var links = vscode.workspace.getConfiguration( 'global-config' ).get( 'links' );
+        let links = vscode.workspace.getConfiguration('global-config').get('links');
 
-        fs.readdir( source, function( err, list )
-        {
-            if( err )
-            {
-                console.log( err );
+        fs.readdir(source, function (err, list) {
+            if (err) {
+                console.log(err);
             }
-            else
-            {
-                list.forEach( function( entry )
-                {
-                    var file = path.join( source, entry );
+            else {
+                list.forEach(function (entry) {
+                    let file = path.join(source, entry);
 
-                    var stat = fs.statSync( file );
-                    if( stat && !stat.isDirectory() )
-                    {
-                        var target = path.join( destination, entry );
-                        if( links.indexOf( entry ) > -1 )
-                        {
-                            if( !fs.existsSync( target ) )
-                            {
-                                fs.symlinkSync( file, target );
+                    let stat = fs.statSync(file);
+                    if (stat && !stat.isDirectory()) {
+                        let target = path.join(destination, entry);
+                        if (links.indexOf(entry) > -1) {
+                            if (!fs.existsSync(target)) {
+                                fs.symlinkSync(file, target);
                             }
                         }
-                        else
-                        {
-                            fs.copySync( file, target, { overwrite: false } );
+                        else {
+                            fs.copySync(file, target, { overwrite: false });
                         }
                     }
-                } );
+                });
             }
-        } );
+        });
     }
 
-    var disposable = vscode.commands.registerCommand( 'global-config.copy', copyConfig );
+    let disposable = vscode.commands.registerCommand('global-config.copy', copyConfig);
 
-    context.subscriptions.push( disposable );
+    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-function deactivate()
-{
+function deactivate() {
 }
 exports.deactivate = deactivate;
